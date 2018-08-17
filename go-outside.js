e1faadc67972e15c4764ebca6450fb7a68c29cce
jeffreypang911 <jeffreypang911@gmail.com>
2018-08-17 01:18:35 +0000

    adding go-outside assignment

---
+++ go-outside.js
@@ -0,0 +1,12 @@
+var weather = 12;
+
+if (weather < 0) {
+  console.log ("wear a coat")
+}
+else if (weather > 22) {
+  console.log ("Tshirt is okay ")
+}
+
+else {
+  console.log ("Sweater Weather!")
+}
