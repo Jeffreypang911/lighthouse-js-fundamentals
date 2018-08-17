9437319d62af3911dad58ecc99ac8e130d975968
jeffreypang911 <jeffreypang911@gmail.com>
2018-08-17 17:42:53 +0000

    added ingredients assignment

---
+++ ingredients.js
@@ -0,0 +1,19 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var item = 0
+
+while (item < ingredients.length) {
+  console.log(ingredients[item])
+  item++;
+}
+
+for (var item = 0; item < 7; item++)
+  console.log(ingredients[item]);
+
+for (var item = 7; item >= 0; item--)
+  console.log(ingredients[item]);
+
+// Write a while loop that prints out the contents of ingredients:
+
+// Write a for loop that prints out the contents of ingredients:
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
